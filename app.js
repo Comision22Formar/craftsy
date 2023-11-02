5724708c99c28646b7c16176aab5b5fdd609e89b
Eric <menaericdaniel@gmail.com>
2023-11-02 09:06:52 -0300

    feat: add initial structure forms login & register

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const PORT = 3030;
 
 app.use(express.static(path.join(__dirname,'public')));
 
-
+/* rutas */
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
-
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
 
 
 
